c28b8f22686c77c49382d7cff648097941fc2b0a
ogi-98 <oguzkaya99@hotmail.com>
2022-03-23 12:50:25 +0300

    Initial Commit

---
+++ SEN4992/SEN4992App.swift
@@ -0,0 +1,20 @@
+//
+//  SEN4992App.swift
+//  SEN4992
+//
+//  Created by Oğuz Kaya on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SEN4992App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
